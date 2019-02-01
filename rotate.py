b9be51426ad11ee97b6b049c1ca6c01963a987c8
Chen Xiao <39661761+ChenXiao61@users.noreply.github.com>
2019-02-01 16:37:36 +0800

    Update rotate.py

--- rotate.py
+++ rotate.py
@@ -3,25 +3,18 @@ import os
 from PIL import Image
 import matplotlib.pyplot as plt
 
-image_size = 224  # 设定尺寸
-source_path = "./0/"  # 源文件路径
-#target_path = "./1/rotate45/"  # 输出目标文件路径
-#target_path = "./1/rotate90/"  # 输出目标文件路径
-#target_path = "./1/rotate135/"  # 输出目标文件路径
-#target_path = "./1/rotate225/"  # 输出目标文件路径
-#target_path = "./1/rotate270/"  # 输出目标文件路径
+source_path = "./0/" 
 target_path = "./r315/"
 
 if not os.path.exists(target_path):
     os.makedirs(target_path)
 
-image_list = os.listdir(source_path)  # 获得文件名
+image_list = os.listdir(source_path) 
 
 i = 0
 for file in image_list:
     i = i + 1
     image = Image.open(source_path + file)
-    #image_source = cv2.imread(source_path + file)  # 读取图片
     im2 = image.rotate(315)
-    im2.save(target_path+file)  # 重命名并且保存
-print("批量处理完成")
+    im2.save(target_path+file)  # 
+print("Finished")
