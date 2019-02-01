fc3dd7feb2368bc2d7f14ef844338ddba218f2b7
Chen Xiao <39661761+ChenXiao61@users.noreply.github.com>
2019-02-01 16:06:18 +0800

    Update batchSpiltGchannel.py

--- batchSpiltGchannel.py
+++ batchSpiltGchannel.py
@@ -3,28 +3,18 @@ import os
 from PIL import Image
 import matplotlib.pyplot as plt
 
-image_size = 224  # 设定尺寸
-source_path = "./2Jan/1/"  # 源文件路径
-#target_path = "./1/rotate45/"  # 输出目标文件路径
-#target_path = "./1/rotate90/"  # 输出目标文件路径
-#target_path = "./1/rotate135/"  # 输出目标文件路径
-#target_path = "./1/rotate225/"  # 输出目标文件路径
-#target_path = "./1/rotate270/"  # 输出目标文件路径
+source_path = "./2Jan/1/"  # path of source file
 target_path = "./2Jan/1_G_Channel/"
 
 if not os.path.exists(target_path):
     os.makedirs(target_path)
 
-image_list = os.listdir(source_path)  # 获得文件名
+image_list = os.listdir(source_path)  # obtain the image in the source file
 
 i = 0
 for file in image_list:
     i = i + 1
-    #RGB = Image.open(source_path + file)
-    RGB = cv2.imread(source_path + file, -1)  # 传入一张彩色图片
-    b, g, r = cv2.split(RGB)  # 分离颜色通道
-    #image_source = cv2.imread(source_path + file)  # 读取图片
-    #im2 = image.rotate(270)
-    #g.save(target_path+file)  # 重命名并且保存
-    cv2.imwrite(target_path+file, g)  # 保存蓝色RGB图像
-print("批量处理完成")
+    RGB = cv2.imread(source_path + file, -1)  # read one image from source file
+    b, g, r = cv2.split(RGB)  # apilt the R,G,B channel of the image
+    cv2.imwrite(target_path+file, g)  # write into target file
+print("Finished")
